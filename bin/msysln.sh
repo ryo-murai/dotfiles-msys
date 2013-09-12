73a4e50e946c024b186071f4d70f3d4d50d74dd7
ryo-murai <murai.ry@gmail.com>
2013-09-12 16:44:10 +0900

    added support for ln options and absolute path

--- bin/msysln.sh
+++ bin/msysln.sh
@@ -1,28 +1,72 @@
 #!/bin/bash
 
 print_usage () {
-    echo "Usage: $0 TARGET [LINK_NAME]"
+  read -d '' USAGE <<EOF
+Usage: $0 [options] TARGET [LINK_NAME]
+
+ options:
+  -s, --symbolic              make symbolic links instead of hard links
+      --help                  display this help and exit
+
+  * other options are not supported *
+
+EOF
+
+  echo "$USAGE"
 }
 
-case "$#" in
-    [1,2])
-        TARGET=$1
-        LINKNAME=${2:-`basename $TARGET`}
-        ;;
+get_nativepath() {
+  echo "$1" | sed -r 's/^\/([a-zA-Z])\//\1:\\/' | sed -r 's/\//\\/g'
+}
+
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    -s|--symbolic)
+      # default behavior
+      shift
+      ;;
+
+    --help)
+      print_usage
+      exit 0
+      ;;
+
+    -*)
+      print_usage
+      exit 1
+      ;;
+
     *)
-        print_usage
-        exit 1
-        ;;
-esac
+      TARGET=$1
+      LINKNAME=${2:-`basename $TARGET`}
+      shift
+      shift
+      ;;
+  esac
+done
+      
+if [ -z "$TARGET" ]; then
+  print_usage
+  exit 1
+fi
+
+if [ ! -e "$TARGET" ]; then
+  echo "$0: accessing \"$TARGET\": No such file or directory" >&2
+  exit 2
+fi
 
 if [ -d "$TARGET" ]; then
   OPT="/D"
 fi
 
-#echo "creating symlink for $TARGET to $LINKNAME"
+if [ -d "$LINKNAME" ]; then
+  LINKNAME="$LINKNAME/"`basename $TARGET`
+fi
 
-TARGETPATH=`echo "$TARGET" | sed -r 's/\//\\\/g'`
-LINKPATH=`echo "$LINKNAME" | sed -r 's/\//\\\/g'`
+echo "creating symlink for $TARGET to $LINKNAME"
+
+TARGETPATH=`get_nativepath "$TARGET"`
+LINKPATH=`get_nativepath "$LINKNAME"`
 
 echo "mklink $OPT $LINKPATH $TARGETPATH"
 cmd //c "mklink $OPT $LINKPATH $TARGETPATH"
