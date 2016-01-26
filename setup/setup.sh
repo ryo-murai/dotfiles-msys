83b04a8ea1f43d8191a583d48f167543575d5d4e
ryo-murai <murai.ry@gmail.com>
2016-01-26 12:00:36 +0900

    update setup scripts

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,6 +2,11 @@
 
 RCDIR=~/dotfiles/rc
 
+case `uname` in
+  MINGW32*) LINKER=~/dotfiles/bin/msysln.sh ;;
+  *) LINKER=ln;;
+esac
+
 cd $RCDIR
 FILES=`find . -type f`
 
@@ -11,6 +16,9 @@ cd ~
 for file in $FILES
 do
   mkdir -p `dirname $file`
-  ln ~/dotfiles/rc/$file $file
+  $LINKER ~/dotfiles/rc/$file $file
 done
+
+# 
+$LINKER -s ~/dotfiles/bin ~/bin
 
--- setup/setupsbt.sh
+++ setup/setupsbt.sh
@@ -2,16 +2,16 @@
 
 download()
 {
-  VERSION=${1:-0.12.3}
+  VERSION=${1:-0.13.9}
 
   echo "target sbt version ${VERSION}"
-  TYPESAFE_REPO=http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch
+  TYPESAFE_REPO=https://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch
   JAR_URL=$TYPESAFE_REPO/${VERSION}/sbt-launch.jar
   JAR=$HOME/bin/sbt-launch.jar
 
   echo "Downloading $JAR"
 
-  curl -f -o $JAR $JAR_URL  
+  curl -L -f -o $JAR $JAR_URL  
   if [ $? -ne 0 ]; then
     rm $JAR
     die "ERROR: $JAR not found"
