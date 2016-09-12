6b53be449de242d4f994d52ef29497b1a9b07496
ryo-murai <murai.ry@gmail.com>
2016-09-12 18:59:44 +0900

    add archive script

---
+++ bin/archive_local_repo.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ ! -d ./.git ]; then
+  echo "run at a git repository"
+  exit 4
+fi
+
+tgzfile=${1:-`basename $PWD`}
+destdir=${2:-~/temp}
+
+postfix=`date +%Y%m%d`
+filepath=${destdir}/${tgzfile}_${postfix}.tgz
+
+if [ -e $filepath ]; then
+  echo "file: $filepath already exist. please check or specify other path / name"
+  echo "usage: $0 [filename [dir]]"
+  exit 6
+fi
+
+tar cvfz ${filepath} `git ls-files`
+
+echo ""
+echo "archive file created: $filepath"
